de8e1bb00a448d8c7731ae1e92d429f485d403ae
Paras0-7 <parasrawat937@gmail.com>
2022-06-12 22:24:04 +0530

    searchfilter

--- src/components/SearchPanel/SearchFilter.js
+++ src/components/SearchPanel/SearchFilter.js
@@ -1,25 +1,23 @@
 import { click } from "@testing-library/user-event/dist/click";
 import { Button } from "../UI/Button";
 import styles from "./css/SearchFilter.module.css";
+import { useState } from "react";
 
 export const SearchFilter = function () {
-  const btn1 = `${styles.actions} ${styles.active} ${styles.btn1}`;
+  const [active, isActive] = useState(false);
+
+  const btn1 = `${styles.actions}  ${styles.btn1}`;
   const btn2 = `${styles.actions} ${styles.btn2}`;
-  const clickHandler = function (event) {
-    console.log(event.target);
-    // const activeBtn = document.querySelector(".active");
-    // activeBtn.classList.remove("active");
-    // event.target.classList.add("active");
-    // console.log("click");
-  };
+  const clickHandler = function (event) {};
+
   return (
     <div className={styles.filter}>
       <p>Search By</p>
       <div>
-        <Button onClick={clickHandler} className={btn1}>
+        <Button id="1" onClick={clickHandler} className={btn1}>
           Title
         </Button>
-        <Button onClick={clickHandler} className={btn2}>
+        <Button id="2" onClick={clickHandler} className={btn2}>
           Genre
         </Button>
       </div>
